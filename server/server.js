46cd4523babf607e0bdb5e6d0df3f9a93a6e979d
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-07-26 16:58:15 -0400

    Clean up

--- server/server.js
+++ server/server.js
@@ -12,64 +12,56 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    // createItem(req.body.text, null, null, null);
-    // console.log(req.body);
-    var todo = new Todo({text: req.body.text});
-
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (err) => {
-        res.status(400).send(err);
-    });
+    res.send(createItem(req.body.text, null, null, null));
 });
 
 app.listen(PORT, () => {
     console.log(`App listening on ${PORT}`);
 });
 
-// /**
-//  * 
-//  * @param {string} text todo text
-//  * @param {boolean} completed true/false
-//  * @param {number} completedAt when completed
-//  * @param {string} email user email
-//  */
-// var createItem = (text, completed, completedAt, email) => {
-//     let objToSave = email ? objectBuilder(null, null, null, email) :
-//             objectBuilder(text, completed, completedAt, null);
-//     let what = email ? 'User' : 'Todo';
+/**
+ * 
+ * @param {string} text todo text
+ * @param {boolean} completed true/false
+ * @param {number} completedAt when completed
+ * @param {string} email user email
+ */
+var createItem = (text, completed, completedAt, email) => {
+    let objToSave = email ? objectBuilder(null, null, null, email) :
+            objectBuilder(text, completed, completedAt, null);
+    let what = email ? 'User' : 'Todo';
 
-//     objToSave.save().then((doc) => {
-//         // doc will have an __v prop, keeps track of model version (any change)
-//         console.log(logToConsole(what, doc));
-//         // process.exit(0);
-//     }, (err) => {
-//         console.log(logToConsole(null, null, err));
-//     });
-// };
+    objToSave.save().then((doc) => {
+        // doc will have an __v prop, keeps track of model version (any change)
+        console.log(logToConsole(what, doc));
+        // process.exit(0);
+    }, (err) => {
+        console.log(logToConsole(null, null, err));
+    });
+};
 
-// /**
-//  * 
-//  * @param {string} text todo text
-//  * @param {boolean} completed true/false
-//  * @param {number} completedAt when completed
-//  * @param {string} email true/false
-//  * 
-//  * @returns {object} object to save
-//  */
-// var objectBuilder = (text, completed, completedAt, email) => {
-//     let obj = email ? new User({email}) : new Todo({text, completed, completedAt});
+/**
+ * 
+ * @param {string} text todo text
+ * @param {boolean} completed true/false
+ * @param {number} completedAt when completed
+ * @param {string} email true/false
+ * 
+ * @returns {object} object to save
+ */
+var objectBuilder = (text, completed, completedAt, email) => {
+    let obj = email ? new User({email}) : new Todo({text, completed, completedAt});
 
-//     return obj;
-// };
+    return obj;
+};
 
-// /**
-//  * 
-//  * @param {string} what what to save
-//  * @param {object} item object saved
-//  */
-// var logToConsole = (what, item, err) => {
-//     log = (what && item) ? `Saving ${what}: ` + JSON.stringify(item, undefined, 4) :
-//             `Unable to create record ${err}`;
-//     return log;
-// };
+/**
+ * 
+ * @param {string} what what to save
+ * @param {object} item object saved
+ */
+var logToConsole = (what, item, err) => {
+    log = (what && item) ? `Saving ${what}: ` + JSON.stringify(item, undefined, 4) :
+            `Unable to create record ${err}`;
+    return log;
+};
