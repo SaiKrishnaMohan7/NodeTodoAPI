369527ef28f02e7fb99471ae4b5b05645decae8d
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-07-26 18:20:51 -0400

    Test for GET /todos route

--- server/tests/sever.test.js
+++ server/tests/sever.test.js
@@ -4,10 +4,13 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo_model');
 
+const todos = [{text: 'Todo x'}, {text: 'Todo Y'}];
 
 // Clean db before every test case
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +27,7 @@ describe('POST /todos', () => {
         .end((err, res) => {
             if(err) return done(err);
             // Check if saved in db
-            Todo.find(/*{text}*/).then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -42,9 +45,21 @@ describe('POST /todos', () => {
             if (err) return done(err);
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(todos.length);
                 done();
             }).catch((err) => done(err));
         });
+    });
+});
+
+describe('GET /todos', (done) => {
+    it('should get all todos from db', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(todos.length);
+        })
+        .end(done);
     });
 });
