137e621c8dbf42221e9f3628470f7532e120aa10
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-07-26 14:40:38 -0400

    Corrected stupid uppercase M

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,4 @@
-var mongoose = require('Mongoose');
+var mongoose = require('mongoose');
 var url = process.env.DB_URL || 'mongodb://localhost:27017/TodoApp';
 // Telling mongoose which Promise lib to use
 mongoose.Promise = global.Promise;
