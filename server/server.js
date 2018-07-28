1b58084c4c94cc0e05f22ce5f45b69a00d084b71
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-07-27 23:27:22 -0400

    User post route added

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 
-const {mongoose, ObjectID} = require('./db/mongoose');
+const {ObjectID} = require('./db/mongoose');
 const PORT = process.env.PORT;
 
 var {User} = require('./models/user_model');
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
     let text = req.body.text;
     let todoObj  =  new Todo({text});
-    let query = {text: text, completed: false};
+    let query = {text, completed: false};
 
     Todo.findOne(query).then((todo) => {
         // Duplicity check
@@ -29,13 +29,13 @@ app.post('/todos', (req, res) => {
         }, (err) => {
             res.status(400).send(err);
         });
-    }, (err) => {
+    }, (/*err*/) => {
         res.send('Something wrong with redundancy check');
     });
 
 });
 
-app.get('/todos', (req, res) => {
+app.get('/todos', (/*req, */ res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (err) => {
@@ -44,7 +44,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-    let {id, isValid} = validator(req.params.id);
+    let {id, isValid} = idValidator(req.params.id);
     
     // valid id check
     if(!isValid) return res.status(404).send('ID not valid');
@@ -62,7 +62,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
-    let {id, isValid} = validator(req.params.id);
+    let {id, isValid} = idValidator(req.params.id);
 
     if(!isValid) return res.status(404).send('Invalid ID');
 
@@ -76,7 +76,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 app.patch('/todos/:id', (req, res) => {
-    let {id, isValid} = validator(req.params.id);
+    let {id, isValid} = idValidator(req.params.id);
     let body = _.pick(req.body, ['text', 'completed']);
 
     if(!isValid) return res.status(404).send('Invalid ID');
@@ -94,7 +94,18 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((/*err*/) => res.status(404).send());
 });
 
-var validator = (id) => {
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let userObj = new User(req.body);
+
+    userObj.save().then((user) => {
+        res.send(user);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+var idValidator = (id) => {
     var isValid = ObjectID.isValid(id);
     return {id, isValid};
 };
