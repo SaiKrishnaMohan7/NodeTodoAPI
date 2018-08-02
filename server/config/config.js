67154ad25b8fbcc1a99710b2f502f1ecf534e7ff
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-08-02 16:54:50 -0400

    Try-catch works now

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' /*|| env === 'test'*/){
-    let config = require('./config.json');
-    var envConfig = config[env];
+if (env === 'development' || env === 'test'){
+    try {
+        let config = require('./config.json');
+        var envConfig = config[env];
+        Object.keys(envConfig).forEach((key) => {
+            process.env[key] = envConfig[key];
+        });
+    }catch(e){console.log('If you are seeing this in test or dev, something is wrong');}
 
-    Object.keys(envConfig).forEach((key) => {
-        process.env[key] = envConfig[key];
-    });
 } 
