d0e33866a10960409f280514ee92b13ef5aa122d
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-08-02 14:21:14 -0400

    try-catch no work

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test'){
-    try{
-        var config = require('./config.json');
-    }
-    catch(e){}
-
-    let envConfig = config[env];
+if (env === 'development' /*|| env === 'test'*/){
+    let config = require('./config.json');
+    var envConfig = config[env];
 
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key];
