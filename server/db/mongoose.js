990e20a90be94bf6d0bd6ab0ec6bbbe02ea0e6f1
AGhost-7 <jonathan.boudreau.92@gmail.com>
2018-08-03 19:31:13 +0000

    Put back in the "useNewUrlParser"

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var {ObjectID} = require('mongodb');
-var url = process.env.MONGODB_URI || 'mongodb://root:test@localhost:27017';
+
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAppTest';
 
 // Telling mongoose which Promise lib to use
 mongoose.Promise = global.Promise;
-mongoose.connect(url, {});
+mongoose.connect(url, { useNewUrlParser: true });
 
 module.exports = {
     mongoose,
